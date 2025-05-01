94ad97ccce39da6f4853edc9c92a00fc2d6f28e9
Gerald <geraldhenrique22@gmail.com>
2025-05-01 20:12:15 -0300

    Adicionado função para olhar o tempo

--- assents/js/script.js
+++ assents/js/script.js
@@ -2,6 +2,7 @@ const horas = document.getElementById("horas");
 const minutos = document.getElementById("minutos");
 const segundos = document.getElementById("segundos");
 
+
 const relogio = setInterval(function time() {
     let dateToday = new Date();
     let h = dateToday.getHours();
@@ -22,3 +23,27 @@ const relogio = setInterval(function time() {
     minutos.textContent = m;
     segundos.textContent = s;
 })
+
+const key = "6b84c61efb72f53ffc7277534c238a7f"
+
+function colocardadosnatela(dados){
+    document.querySelector(".city").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+async function buscarcidade(cidade){
+    
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`)
+    .then((response) => response.json())
+
+    colocardadosnatela(dados)
+}
+
+function buttonclick(){
+    const cidade = document.querySelector(".input-city").value;
+
+    buscarcidade(cidade)
+}
